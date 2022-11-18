d2d89b6e40ddacb8155c19f12dd719515dd7b490
Alberto Lucchesi <albertolucchesi97@gmail.com>
2022-11-18 17:29:59 +0100

    Update videogames_data.sql
    aggiornamento script sql

--- scripts/videogames_data.sql
+++ scripts/videogames_data.sql
@@ -53,10 +53,130 @@ INSERT INTO videogames (title, release_date, genre, software_house, publisher, s
 VALUES ('GTA V', '2013-09-17', 'Open World', 'Rockstar Games', 'Take Two', 'Live the lifes of three criminals of Los Santos: Mike, Franklin and Trevor', 'https://static-it.gamestop.it/images/products/304447/3max.jpg', 'https://www.youtube.com/embed/hvoD7ehZPcM')
 
 INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
-VALUES ('Mass Effect Legendary Edition', '2021-05-14', 'GDR', 'Bioware', 'EA', 'Play three masterpicies that created the famous series of Mass Effect', 'https://static-it.gamestop.it/images/products/302639/3max.jpg', 'https://www.youtube.com/embed/n8i53TtQ6IQ')
+VALUES ('Mass Effect Legendary Edition', '2020-05-14', 'GDR', 'Bioware', 'EA', 'Play three masterpicies that created the famous series of Mass Effect', 'https://static-it.gamestop.it/images/products/302639/3max.jpg', 'https://www.youtube.com/embed/n8i53TtQ6IQ')
 
 INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
 VALUES ('Until Dawn', '2015-08-25', 'Horror', 'Supermassive Games', 'Sony Entertainment', 'It is winter, and a group of friends reunite in a chalet on the mountains...what will happen?', 'https://static-it.gamestop.it/images/products/260536/3max.jpg', 'https://www.youtube.com/embed/3NcF7EOnjow')
 
 INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
 VALUES ('The Quarry', '2022-06-10', 'Horror', 'Supermassive Games', '2K', 'A group of nine friends go to camping together...', 'https://static-it.gamestop.it/images/products/307911/3max.jpg', 'https://www.youtube.com/embed/Zh2K7SxRHmo')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('God of War Ragnarok', '2022-11-09', 'Adventure', 'Sony Santa Monica', 'Sony Computer Entertainment', 'Kill Thor in the sequel of the acclaimed God of War.', 'https://static-it.gamestop.it/images/products/309576/3max.jpg', 'https://www.youtube.com/embed/hfJ4Km46A-0')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('God of War', '2018-04-20', 'Adventure', 'Sony Santa Monica', 'Sony Computer Entertainment', 'You are Kratos, and along with Atreus you will be start a new journey.', 'https://static-it.gamestop.it/images/products/271893/3max.jpg', 'https://www.youtube.com/embed/K0u_kAWLJOA')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Uncharted 4', '2016-05-10', 'Adventure', 'Naughty Dog', 'Sony Computer Entertainment', 'This is the last ride of Nathan Drake.', 'https://m.media-amazon.com/images/I/51H5VPqbm+L._AC_.jpg', 'https://www.youtube.com/embed/hh5HV4iic1Y')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Uncharted 3', '2011-11-01', 'Adventure', 'Naughty Dog', 'Sony Computer Entertainment', 'This is the third installment of Uncharted.', 'https://static-it.gamestop.it/images/products/151397/3max.jpg', 'https://www.youtube.com/embed/zN3rj6YemkI')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Uncharted The Lost Legacy', '2017-08-22', 'Adventure', 'Naughty Dog', 'Sony Computer Entertainment', 'Follow Chloe and Nadine in this new adventure!', 'https://static-it.gamestop.it/images/products/279341/3max.jpg', 'https://www.youtube.com/embed/-PhhiKPHTWM')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Call of Duty Modern Warfare 2', '2022-10-28', 'FPS', 'Infinity Ward', 'Activision Blizzard', 'Save the USA from an enemy attack!', 'https://static-it.gamestop.it/images/products/308427/3max.jpg', 'https://www.youtube.com/embed/OeVapCrI1pY')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Assetto Corsa Competizione', '2022-02-24', 'Racing', 'Kunos Simulazioni', '505 Games', 'Welcome to the best racing simulator!', 'https://static-it.gamestop.it/images/products/305103/3max.jpg', 'https://www.youtube.com/embed/iZdwW_JqN9k')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Dirt Rally 2', '2020-03-20', 'Racing', 'Codemasters', 'EA', 'Here s the best rally game out there!', 'https://static-it.gamestop.it/images/products/297905/3max.jpg', 'https://www.youtube.com/embed/uwVIp4nFRS8')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Grid Legends', '2022-02-25', 'Racing', 'Codemasters', 'EA', 'Race against racing legends!', 'https://static-it.gamestop.it/images/products/306537/3max.jpg', 'https://www.youtube.com/embed/1Vgj1I3YG8g')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Battlefield 2042', '2021-11-19', 'FPS', 'DICE', 'EA', 'This is the new Battlefield!', 'https://static-it.gamestop.it/images/products/302226/3max.jpg', ' https://www.youtube.com/embed/ASzOzrB-a9E')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Dark Souls Remastered', '2018-05-24', 'GDR', 'From Software', 'Bandai Namco', 'This is the first chapter of the beloved franchise of Dark Souls!', 'https://static-it.gamestop.it/images/products/285867/3max.jpg', 'https://www.youtube.com/embed/Woe0PYZPxRw')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Dark Souls 2', '2014-03-13', 'GDR', 'From Software', 'Bandai Namco', 'This is the second chapter of the beloved franchise of Dark Souls!', 'https://static-it.gamestop.it/images/products/255285/3max.jpg', 'https://www.youtube.com/embed/U6uyuIQYlfY')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Dark Souls 3', '2016-05-17', 'GDR', 'From Software', 'Bandai Namco', 'This is the third chapter of the beloved franchise of Dark Souls!', 'https://static-it.gamestop.it/images/products/271190/3max.jpg', 'https://www.youtube.com/embed/cWBwFhUv1-8')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Demon s Souls', '2020-11-12', 'GDR', 'Bluepoint Games', 'Sony Computer Entertainment', 'This is the remake of the first Souls game, that started it all!', 'https://static-it.gamestop.it/images/products/298910/3max.jpg', 'https://www.youtube.com/embed/2TMs2E6cms4')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('The Witcher 3', '2015-05-19', 'GDR', 'CD Project RED', 'Bandai Namco', 'The end of the beautiful franchise of The Witcher. You are Geralt, save Ciri!', 'https://static-it.gamestop.it/images/products/258017/3max.jpg', 'https://www.youtube.com/embed/BCDtAHPzsO4')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Cyberpunk 2077', '2020-12-10', 'GDR', 'CD Project RED', 'Bandai Namco', 'You are V, and you will have to embark on a journey to save yourself!', 'https://static-it.gamestop.it/images/products/292740/3max.jpg', 'https://www.youtube.com/embed/JFf8I_8Ubv4')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('The Dark Pictures Anthology: Man of Medan', '2019-08-30', 'Horror', 'Supermassive Games', 'Bandai Namco', 'Discover what lies behind a ghost ship. Is it really haunted?', 'https://static-it.gamestop.it/images/products/290050/3max.jpg', 'https://www.youtube.com/embed/OQvo4-Ly-sA')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('The Dark Pictures Anthology: Little Hope', '2020-10-30', 'Horror', 'Supermassive Games', 'Bandai Namco', 'A group of students ends up in Little Hope, a strange small city. What is going on?', 'https://static-it.gamestop.it/images/products/298415/3max.jpg', 'https://www.youtube.com/embed/XTV6xbsOQPA')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('The Dark Pictures Anthology: House of Ashes', '2021-10-22', 'Horror', 'Supermassive Games', 'Bandai Namco', 'A group of soldiers ends up in a mine...and there is a monster.', 'https://static-it.gamestop.it/images/products/304189/3max.jpg', 'https://www.youtube.com/embed/QBZgZdC989c')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('The Dark Pictures Anthology: The Devil in Me', '2022-11-18', 'Horror', 'Supermassive Games', 'Bandai Namco', 'A troupe get invited in an hotel, inspired by the one that HH Holmes used for his murders...', 'https://static-it.gamestop.it/images/products/309918/3max.jpg', 'https://www.youtube.com/embed/UdeeI4Zvnio')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Resident Evil 2', '2019-01-25', 'Horror', 'CAPCOM', 'CAPCOM', 'The remake of the famous Resident Evil 2!', 'https://static-it.gamestop.it/images/products/288011/3max.jpg', 'https://www.youtube.com/embed/u3wS-Q2KBpk')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('The Callisto Protocol', '2022-12-02', 'Horror', 'Striking Distance', 'Krafton', 'You are a prisoner on the Callisto moon...', 'https://static-it.gamestop.it/images/products/309106/3max.jpg', 'https://www.youtube.com/embed/tPHWaFRTqFA')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Dead Space', '2023-01-27', 'Horror', 'Motive', 'Microsoft Studios', 'The remake of the famous Dead Space!', 'https://static-it.gamestop.it/images/products/311271/3max.jpg', 'https://www.youtube.com/embed/cTDJNZ9cK1w')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Halo: The Master Chief Collection', '2014-11-11', 'FPS', '343 Industries', 'Microsoft Studios', 'The collection of all the Halo games!', 'https://static-it.gamestop.it/images/products/258107/3max.jpg', 'https://www.youtube.com/embed/ZDvYJGquXgE')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Call of Duty Black Ops Cold War', '2020-11-20', 'FP', 'Treyarch Studios', 'Activision Blizzard', 'The Black Ops saga returns!', 'https://static-it.gamestop.it/images/products/300173/3max.jpg', 'https://www.youtube.com/embed/aTS9n_m7TW0')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Titanfall 2', '2016-10-28', 'FPS', 'Respawn Entertainment', 'EA', 'Robot and soldiers...together!', 'https://static-it.gamestop.it/images/products/270735/3max.jpg', 'https://www.youtube.com/embed/nHvoaAAhGno')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Call of Duty Vanguard', '2021-11-05', 'FPS', 'Sledgehammer', 'Activision Blizzard', 'A jump back in the Second World War!', 'https://static-it.gamestop.it/images/products/305038/3max.jpg', 'https://www.youtube.com/embed/WWysm-dPkL4')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Call of Duty 4 Modern Warfare', '2007-11-09', 'FPS', 'Infinity Ward', 'Activision Blizzard', 'The first CoD game set in the modern warfare!', 'https://static-it.gamestop.it/images/products/136410/3max.jpg', 'https://www.youtube.com/embed/LhuIjNSg7Gg')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Hitman 3', '2021-01-20', 'Stealth', 'IO Interactive', 'IO Interactive', 'New missions with the most lethal killer, Agent 47', 'https://static-it.gamestop.it/images/products/298926/3max.jpg', 'https://www.youtube.com/embed/R_Ob-fupzKg')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Metal Gear Solid V: The Phantom Pain', '2015-09-07', 'Stealth', 'KONAMI', 'KONAMI', 'The end of the Metal Gear Saga', 'https://static-it.gamestop.it/images/products/264781/3max.jpg', 'https://www.youtube.com/embed/C19ap2M7DDE')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Dishonored 2', '2016-11-11', 'Stealth', 'Arkane Studios', 'Bethesda', 'Get back in the world of Dishonored!', 'https://static-it.gamestop.it/images/products/274566/3max.jpg', 'https://www.youtube.com/embed/SM-dBb4aR9M')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Mirror s Edge Catalyst', '2016-06-09', 'Open World', 'DICE', 'EA', 'The second chapter of Mirror s Edge!', 'https://static-it.gamestop.it/images/products/263444/3max.jpg', 'https://www.youtube.com/embed/vOe3ik_RViE')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Grand Theft Auto: The Trilogy - The Definitive Edition', '2021-12-17', 'Open World', 'Rockstar Games', 'Take Two', 'The definitive edition of the GTA trilogy!', 'https://static-it.gamestop.it/images/products/306085/3max.jpg', 'https://www.youtube.com/embed/vOe3ik_RViE')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Forza Motorsport 7', '2017-10-03', 'Racing', 'Turn 10', 'Microsoft Studios', 'Race in the new chapter of the Forza franchise!', 'https://static-it.gamestop.it/images/products/280551/3max.jpg', 'https://www.youtube.com/embed/9aAr5blVy0g')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Forza Motosport 6', '2015-09-18', 'Racing', 'Turn 10', 'Microsoft Studios', 'The sixth game of the Forza franchise!', 'https://static-it.gamestop.it/images/products/260822/3max.jpg', 'https://www.youtube.com/embed/YPziu4TBLts')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Forza Motorsport 5', '2013-12-20', 'Racing', 'Turn 10', 'Microsoft Studios', 'The fifth game of the Forza franchise!', 'https://static-it.gamestop.it/images/products/254584/3max.jpg', 'https://www.youtube.com/embed/YLcjfALaQh0')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Marvel s Spiderman', '2018-09-07', 'Open World', 'Insomniac', 'Sony Computer Entertainment', 'The first game of the beautiful Spiderman universe!', 'https://static-it.gamestop.it/images/products/279231/3max.jpg', 'https://www.youtube.com/embed/3R2uvJqWeVg')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Marvel s Spiderman Miles Morales', '2020-11-12', 'Open World', 'Insomniac', 'Sony Computer Entertainment', 'The follow up of Marvel s Spiderman, with Miles Morales!', 'https://static-it.gamestop.it/images/products/298902/3max.jpg', 'https://www.youtube.com/embed/NTunTURbyUU')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Days Gone', '2019-04-26', 'Open World', 'Bend Studio', 'Sony Computer Entertainment', 'Deacon St. John must survive and live in this world full of hordes of zombies', 'https://static-it.gamestop.it/images/products/279229/3max.jpg', 'https://www.youtube.com/embed/FKtaOY9lMvM')
+
+INSERT INTO videogames (title, release_date, genre, software_house, publisher, synopsis, cover, trailer)
+VALUES ('Ghost of Tsushima Director s Cut', '2021-08-20', 'Open World', 'Sucker Punch', 'Sony Computer Entertainment', 'Live like a ghost in this beautiful representation of Japan', 'https://static-it.gamestop.it/images/products/302238/3max.jpg', 'https://www.youtube.com/embed/gxEmQHWzT90')
